5ebc62bfcfd86eb0de280ba4d8ca47639af30152
Ulanbek <m.u.u.91@mail.ru>
2021-05-21 16:59:55 +0600

    Домашка №18

---
+++ js/script.js
@@ -0,0 +1,161 @@
+// ========================================
+// Home work 23 
+
+// let my_age = 29;
+// let my_year = 1991;
+// let my_name = "Ulanbek";
+
+// console.log('Мне в данный момент ' + my_age + ' лет');
+// console.log('Я ' + my_year + ' года рождения');
+// console.log('Меня зовут ' + my_name);
+
+// ========================================
+// Home work 24
+// Objects
+// let person = {
+//     name: 'Ulanbek',
+//     sirname: 'Moidun uulu',
+//     age: 29,
+//     date_of_birth: 1991,
+//     place_birth: 'Бишкек',
+//     profession: 'Frontend Developers',
+//     height: 176,
+//     weight: 80
+// };
+
+
+// console.log('Имя: ' + person['name'] + '\n');
+// console.log('Фамилия: ' + person['sirname'] + '\n');
+// console.log('Возраст: ' + person['age'] + '\n');
+// console.log('Год рождения: ' + person['date_of_birth'] + '\n');
+// console.log('Место рождения: ' + person['place_birth'] + '\n');
+// console.log('Профессия: ' + person['profession'] + '\n');
+// console.log('Рост: ' + person['height'] + ' см' + '\n');
+// console.log('Вес: ' + person['weight'] + ' кг' + '\n' + '\n' + '\n');
+
+// --------------------
+// Arrays
+
+// let array1 = ['Мерс', 'BMW', 'Toyota', 'Lada', 'Трактор', ['yamaha', 'honda', 'suzuki', 'HD']]
+
+// console.log('Марка машины: ' + array1[0] + '\n');
+// console.log('Марка машины: ' + array1[1] + '\n');
+// console.log('Марка машины: ' + array1[2] + '\n');
+// console.log('Марка машины: ' + array1[3] + '\n');
+// console.log('Марка машины: ' + array1[4] + '\n');
+// console.log('Марка мотоцикла: ' + array1[5][0] + '\n');
+// console.log('Марка мотоцикла: ' + array1[5][1] + '\n');
+// console.log('Марка мотоцикла: ' + array1[5][2] + '\n');
+// console.log('Марка мотоцикла: ' + array1[5][3] + '\n');
+
+// ========================================
+// Home work 25
+// --------------------
+
+// let yourName = prompt('Введите ваше имя: ', '');
+// let yourSirname = prompt('И фамимлию тоже: ', '');
+
+// console.log('Ваше Имя и Фамилия: ' + yourName + ' sfdvdfvgrt' + yourSirname + '\n' + '\n');
+
+// --------------------
+// let num1 = prompt('Введите первое число для сложения: ', '');
+// let num2 = prompt('Теперь введите второе число: ', '');
+// let result = Number(num1) + Number(num2);
+
+// alert('Результат сложения чисел равна: 6262' + result);
+
+// --------------------
+// let number1 = prompt('Введите первое число для умножения: ', '');
+// let number2 = prompt('Теперь введите второе число: ', '');
+// let result2 = Number(number1) * Number(number2);
+
+// alert('Результат умножения чисел равна: ' + result2);
+
+// ========================================
+// Home work 26
+// --------------------
+// Сложение
+// let num1 = prompt('Введите первое число для сложения: ', '');
+// let num2 = prompt('Теперь введите второе число: ', '');
+// let result = Number(num1) + Number(num2);
+
+// alert('Результат сложения чисел равна: ' + result);
+
+// --------------------
+// Вычитание
+// let num1_2 = prompt('Введите первое число для вычитания: ', '');
+// let num2_2 = prompt('Теперь введите второе число: ', '');
+// let result2 = Number(num1_2) - Number(num2_2);
+
+// alert('Результат вычитания чисел равна: ' + result2);
+// --------------------
+// Умножение
+// let num1_3 = prompt('Введите первое число для умножения: ', '');
+// let num2_3 = prompt('Теперь введите второе число: ', '');
+// let result3 = Number(num1_3) * Number(num2_3);
+
+// alert('Результат умножения чисел равна: ' + result3);
+
+// --------------------
+// Деление
+// let num1_4 = prompt('Введите первое число для деления: ', '');
+// let num2_4 = prompt('Теперь введите второе число: ', '');
+// let result4 = Number(num1_4) / Number(num2_4);
+
+// alert('Результат деления чисел равна: ' + result4);
+
+// --------------------
+// Остаток от деления 
+// let num1_5 = prompt('Введите первое число, чтобы узнать остаток от деления: ', '');
+// let num2_5 = prompt('Теперь введите второе число: ', '');
+// let result5 = Number(num1_5) % Number(num2_5);
+
+// alert('Результат отстатка от деления равна: ' + result5);
+
+// --------------------
+// Возведение в степень
+// let num1_6 = prompt('Введите первое число, которое хотите возвести в степень: ', '');
+// let num2_6 = prompt('Теперь введите число, в какую степень хотите возвести: ', '');
+// let result6 = Math.pow(Number(num1_6), Number(num2_6));
+
+// alert('Результат возведения в степень: ' + result6);
+// --------------------
+// --------------------
+
+// let login = prompt('Введите ваш логин: ', '');
+// let pass = prompt('Введите ваш пароль: ', '');
+
+// let myLogin = 'admin';
+// let myPass = 123;
+
+// if (login == myLogin && pass == myPass) {
+//     alert('Ваш Логин и Пароль верный!')
+// } else if (login == myLogin && pass !== myPass) {
+//     alert('Ваш Логин верный, но Пароль не верный!')
+
+// } else if (login !== myLogin && pass == myPass) {
+//     alert('Ваш Логин не верный, а Пароль верный!')
+
+// } else if (login !== myLogin && pass !== myPass) {
+//     alert('Ваш Логин и Пароль не верный!')
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ========================================
