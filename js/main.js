cf7d1c7871cadfdd20e985caaca81fe350474071
Ulanbek <m.u.u.91@mail.ru>
2021-05-27 13:12:24 +0600

    Домашка Урока №31

--- js/main.js
+++ js/main.js
@@ -64,32 +64,61 @@ btnBasket[2].addEventListener("click", function (event) {
 // -------------------
 // Вичисляем длину окружности, когда вводим радиус окружности
 
-function lengthCircle(radius) {
-  return 2 * Math.PI * radius;
-}
-var lengthCircleResults = lengthCircle(20);
-console.log('Длина окружности равна: ' + lengthCircleResults);
+// function lengthCircle(radius) {
+//   return 2 * Math.PI * radius;
+// }
+// var lengthCircleResults = lengthCircle(20);
+// console.log('Длина окружности равна: ' + lengthCircleResults);
 
+// // -------------------
+// // Функцию  которая возвращает меньшее из чисел a и b
+
+// function min(a, b) {
+//   if (a < b) {
+//     return a;
+//   } else {
+//     return b;
+//   }
+// }
+// var lesserNumber = min(5, 10);
+
+// console.log('Меньшее число: ' + lesserNumber);
 // -------------------
-// Функцию  которая возвращает меньшее из чисел a и b
-
-function min(a, b) {
-  if (a < b) {
-    return a;
-  } else {
-    return b;
-  }
-}
-var lesserNumber = min(5, 10);
-
-console.log('Меньшее число: ' + lesserNumber);
 
 // ========================================
+// Домашка №31
+// -------------------
+// 1) Возводим в степень числа вводимые пользователем
 
+function powerNum(number) {
+  return Math.pow(number, 2);
+}
+let numberPow = 20;
+let powerResult = powerNum(numberPow);
 
+console.log('Результат возведения в степени числа ' + numberPow + ' равна: ' + powerResult);
 
+// -------------------
+// 2) Находим площадь круга, радиус который вводит пользователь
 
+function areaCircle(radius) {
+  return Math.pow(radius, 2) * Math.PI;
+}
+let areaCircleUser = 5;
+let areaCircleRes = areaCircle(areaCircleUser);
+console.log('Площадь круга c радиусом ' + areaCircleUser + ' равна: ' + areaCircleRes);
 
+// -------------------
+// 3) Находим площадь прямоугольника
+
+function areaRectangle(lengthRectangle, widthRectangle) {
+  return lengthRectangle * widthRectangle;
+}
+let areaRectangleLength = 8;
+let areaRectangWidth = 6;
+
+let areaRectangleResult = areaCircle(areaRectangleLength, areaRectangWidth);
+console.log('Площадь прямоугольника с длиной ' + areaRectangleLength + ' и шириной ' + areaRectangWidth + ' равна: ' + areaRectangleResult);
 
 // ========================================
 
