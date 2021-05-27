5c8790ed95b43147af3b9d9b1f8309baba3a8d54
Ulanbek <m.u.u.91@mail.ru>
2021-05-27 10:57:45 +0600

    Урок №29

--- js/main.js
+++ js/main.js
@@ -9,11 +9,62 @@ basket.onclick = function () {
 }
 // ========================================
 // Получение данных о товаре: картинку, заголвок, цены
-var featImg = document.querySelector('.f__item__img > img');
-var featH4 = document.querySelector('.featured__item > h4');
-var featPrice = document.querySelector('.f__item__prices > span');
-var featDel = document.querySelector('.f__item__prices > del');
-// console.log(featDel);
+var featImg = document.querySelectorAll('.f__item__img > img');
+var featH4 = document.querySelectorAll('.featured__item > h4');
+var featPrice = document.querySelectorAll('.f__item__prices > span');
+var featDel = document.querySelectorAll('.f__item__prices > del');
+// console.log(featImg.src);
+
+// ========================================
+// Перебор товаров
+
+// featImg.forEach(img => console.log(img));
+// featH4.forEach(h4 => console.log(h4));
+// featPrice.forEach(price => console.log(price));
+// featDel.forEach(del => console.log(del));
+
+// ========================================
+
+var btnBasket = document.querySelectorAll('.f__item__links > .btn__basket');
+console.log(btnBasket);
+
+// По клику получаем название товара
+btnBasket[0].addEventListener("click", function (event) {
+  event.preventDefault();
+  var featH4Length = featH4.length;
+  for (var i = 0; i < featH4Length; i++) {
+    console.log(featH4[i].innerText);
+  }
+});
+// По клику получаем цену товара
+
+btnBasket[1].addEventListener("click", function (event) {
+  event.preventDefault();
+  var featPriceLength = featPrice.length;
+  for (var i = 0; i < featPriceLength; i++) {
+    console.log(featPrice[i].innerText);
+  }
+
+});
+
+// По клику получаем адрес ссылки изображения
+
+btnBasket[2].addEventListener("click", function (event) {
+  event.preventDefault();
+  var featImgLength = featImg.length;
+  for (var i = 0; i < featImgLength; i++) {
+    console.log(featImg[i].src);
+  }
+
+});
+
+// ========================================
+
+
+
+
+
+
 // ========================================
 
 
